8eab757d02e2cedefd481862e8b189f80c85e88f
Henry <jm25002@esfe.agape.edu.sv>
2025-06-02 16:20:26 -0600

    Se encapsulo la clase

--- Producto.java
+++ Producto.java
@@ -1,3 +1,31 @@
 public class Producto {
+  private int id;
+    private String nombre;
+    private String marca;
+    private double precio;
 
+    // Constructor
+    public Producto(int id, String nombre, String marca, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
 }
